77e45c56358d8639dfec9e065f60ed94d83be005
Simon M <smaishman@gmail.com>
2015-01-04 22:23:12 +0000

    Added team names

--- bbcFootballScraper.py
+++ bbcFootballScraper.py
@@ -5,8 +5,16 @@ print footballResults
 #parse the html with beautiful soup
 soupPage = bs4.BeautifulSoup(footballResults.text)
 ##get all of the match details
+
 matchDetails = soupPage.select('td.match-details')
 #scores = soupPage.select('span.score')
-scores = soupPage.find_all(title = 'Score')
-for s in scores:
-	print s.getText()
+for md in matchDetails:
+	print md.select('span.team-home')[0].getText().strip()
+	#print md
+	print md.find(title = 'Score').getText().strip()
+	print md.select('span.team-away')[0].getText().strip()
+	print '--------'
+#scores = soupPage.find_all(title = 'Score')
+
+#for s in scores:
+#	print s.getText()
